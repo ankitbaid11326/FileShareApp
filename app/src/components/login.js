fcd06affc15a0b2c64ac4b7e7ba48e55558bf609
Ankit Baid <ankitbaid11326@gmail.com>
2018-02-27 19:31:35 +0530

    Added Signup UI

--- app/src/components/login.js
+++ app/src/components/login.js
@@ -2,7 +2,18 @@ import React,{Component} from 'react';
 
 class LoginForm extends Component{
     
+    constructor(props){
+        super(props);
+
+        this.state = {
+            isLogin: true,
+        }
+
+    }
+
     render(){
+
+        const {isLogin} = this.state;
 
         return(
 
@@ -15,6 +26,14 @@ class LoginForm extends Component{
                     }} className={'app-dismiss-button'}> Close </button>
                     <h2 className={'form-title'}> Sign In </h2>
                     <form>
+                        {
+                            !isLogin ? <div> 
+                                <div className={'app-form-item'}>
+                                    <label htmlFor={'name-id'}>Enter Name </label>
+                                    <input placeholder={'Enter name'} type={'text'} id={'name-id'} name={'name'} />
+                                </div>
+                            </div> : null
+                        }
                         <div className={'app-form-item'}>
                             <label htmlFor={'email-id'}> Email </label>
                             <input placeholder={'Enter Email'} type={'email'} id={'email-id'} name={'email'} />
@@ -25,11 +44,24 @@ class LoginForm extends Component{
                             <input placeholder={'Enter Password'} type={'password'} id={'password-id'} name={'password'} />
                         </div>
 
+                        {
+                            !isLogin ? <div> 
+                                <div className={'app-form-item'}>
+                                    <label htmlFor={'confirm-password-id'}>Confirm Password </label>
+                                    <input placeholder={'Confirm Password'} type={'password'} id={'confirm-password-id'} name={'confirm-password'} />
+                                </div>
+                            </div> : null
+                        }
+
                         <div className={'app-form-actions'}>
                             <button className={'app-button primary'}> Sign In </button>
                             <div className={'app-form-description'}>
                                 <div> Don't have an account ? </div>
-                                <button className={'app-button signupButton'}> Sign Up </button>
+                                <button onClick={ () => {
+                                    this.setState({
+                                        isLogin: false
+                                    });
+                                }} className={'app-button signupButton'} type={'button'} > Sign Up </button>
                             </div>
                         </div>
 
