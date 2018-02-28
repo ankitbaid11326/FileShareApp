694bf605710d88058394e0c0ad71763b9a422553
Ankit Baid <ankitbaid@Ankits-MBP.Dlink>
2018-03-01 01:03:14 +0530

    Validation for Sign-in and Sign-up is done

--- app/src/components/login.js
+++ app/src/components/login.js
@@ -1,4 +1,7 @@
 import React,{Component} from 'react';
+import classNames from 'classnames';
+import _ from 'lodash';
+import {isEmail} from '../helpers/email';
 
 class LoginForm extends Component{
     
@@ -12,16 +15,115 @@ class LoginForm extends Component{
                 email: '',
                 password: '',
                 confirmPassword: ''
+            },
+            error: {
+                name: null,
+                email: null,
+                password: null,
+                confirmPassword: null,
             }
         }
         this._onSubmit = this._onSubmit.bind(this);
         this._onTextFieldChange = this._onTextFieldChange.bind(this);
+        this._formValidation = this._formValidation.bind(this);
+
+    }
+
+    _formValidation(fieldsToValidate = [] ,callback = () => {}){
+        const {isLogin, user, error} = this.state;
+
+        const allFields = {
+            name : {
+                message: 'Your name is required',
+                doValidate: () => {
+                    const value = _.trim(_.get(user, 'name', ""));
+                    if(value){
+                        return true;
+                    }
+                    return false;
+                }
+            },
+            email: {
+                message: 'Email is required',
+                doValidate: () => {
+                    const value = _.get(user, 'email', '');
+                    if(value.length > 0 && isEmail(value)){
+                        return true;
+                    }
+                    return false;
+                }
+            },
+            password: {
+                message: 'Password should have more than 3 characters',
+                doValidate: () => {
+                    const value = _.get(user, 'password', '');
+                    if(value && value.length > 3){
+                        return true;
+                    }
+                    return false;
+                }
+            },
+            confirmPassword: {
+                message: 'Password does not match',
+                doValidate: () => {
+                    const passwordValue = _.get(user, 'password', '');
+                    const value = _.get(user, 'confirmPassword', '');
+                    if(passwordValue === value){
+                        return true;
+                    }
+                    return false;
+                }
+            }
+        }
+
+        let errors = this.state.error;
+        _.each(fieldsToValidate, (field) => {
+            const fieldValidate = _.get(allFields, field);
+            if(fieldValidate){
+                
+                errors[field] = null;
+                const isFieldValid = fieldValidate.doValidate();
+                if(isFieldValid === false){
+                    errors[field] = _.get(fieldValidate, 'message');
+                }
+            }
+        });
+
+        this.setState({
+            error: errors,
+        }, () => {
+            console.log('after process validation the form errors', errors);
+            let isValid = true;
+            _.each(errors, (err) => {
+                if(err){
+                    isValid = false;
+                }
+            });
+
+            callback(isValid);
+
+        });
 
     }
 
     _onSubmit(event){
-        const {isLogin} = this.state;
+        const {isLogin, user} = this.state;
         event.preventDefault();
+
+        let fieldNeedToValidate = ['email', 'password'];
+
+        if(!isLogin){
+            fieldNeedToValidate = ['name', 'email', 'password', 'confirmPassword'];
+        }
+
+
+        this._formValidation(fieldNeedToValidate, (isValid) => {
+            
+            if(isValid){
+                // send this data to server
+            }
+
+        });
 
     }
 
@@ -37,12 +139,10 @@ class LoginForm extends Component{
 
     render(){
 
-        const {isLogin, user} = this.state;
-
+        const {isLogin, user, error} = this.state;
         const title = isLogin ? 'Sign In' : 'Sign Up';
 
         return(
-
             <div className={'app-login-form'}>
                 <div className={'app-login-form-inner'}>
                     <button  onClick={ () => {
@@ -54,25 +154,25 @@ class LoginForm extends Component{
                     <form onSubmit={this._onSubmit}>
                         {
                             !isLogin ? <div> 
-                                <div className={'app-form-item'}>
+                                <div className={classNames('app-form-item', {'error': _.get(error, 'name')})}>
                                     <label htmlFor={'name-id'}>Enter Name </label>
                                     <input value={user.name} onChange={this._onTextFieldChange} placeholder={'Enter name'} type={'text'} id={'name-id'} name={'name'} />
                                 </div>
                             </div> : null
                         }
-                        <div className={'app-form-item'}>
+                        <div className={classNames('app-form-item', {'error': _.get(error, 'email')})}>
                             <label htmlFor={'email-id'}> Email </label>
                             <input value={user.email} onChange={this._onTextFieldChange} placeholder={'Enter Email'} type={'email'} id={'email-id'} name={'email'} />
                         </div>
                         
-                        <div className={'app-form-item'}>
+                        <div className={classNames('app-form-item', {'error': _.get(error, 'password')})}>
                             <label htmlFor={'password-id'}> Password </label>
                             <input value={user.password} onChange={this._onTextFieldChange} placeholder={'Enter Password'} type={'password'} id={'password-id'} name={'password'} />
                         </div>
 
                         {
                             !isLogin ? <div> 
-                                <div className={'app-form-item'}>
+                                <div className={classNames('app-form-item', {'error': _.get(error, 'confirmPassword')})}>
                                     <label htmlFor={'confirm-password-id'}>Confirm Password </label>
                                     <input value={user.confirmPassword} onChange={this._onTextFieldChange} placeholder={'Confirm Password'} type={'password'} id={'confirm-password-id'} name={'confirmPassword'} />
                                 </div>
---
+++ app/src/helpers/email.js
@@ -0,0 +1,4 @@
+export const isEmail = (email) => {
+    const emailRegex = /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+    return emailRegex.test(email);
+}
